270a41bbeaafab6dcc2c98b4466a4b6b5ab809e5
Amir Hossein Ekhlasi <amir.ekhlasi@gmail.com>
2018-12-31 16:36:15 +0330

    init

---
+++ API.py
@@ -0,0 +1,127 @@
+import HP
+import pickle
+import os
+from gutenberg_book import *
+from paragraph import *
+
+
+def get_books(books_list=None, books_features=None, book_object=True):
+    """
+
+    Args:
+        books_list: (Optional) list of books gutenberg ID to create books. if it is None then it will return all books.
+        books_features: (Optional) features to get books with that feature. a dictionary which values are set or list.
+        book_object: if it is True then returns GutenbergBooks else it will return metadata
+
+    Returns:
+         a dictionary of books objects {id: GutenbergBook(id)/metadata(id)}
+    """
+    if books_list is not None and books_features is not None:
+        raise AttributeError("only one of books_list and books_features should be identified")
+    if os.path.isfile(HP.BOOKS_DATA_PATH):
+        pk = open(HP.BOOKS_DATA_PATH, "rb")
+        books_metadata = pickle.load(pk)
+        pk.close()
+        assert isinstance(books_metadata, dict)
+
+    else:
+        books_metadata = dict()
+
+    if books_list is not None:
+        books_list = books_list & set(books_metadata)
+        books_metadata = {id: books_metadata[id] for id in books_list}
+    if books_features is not None:
+        for feature, items in books_features.items():
+            books_metadata = {id: metadata for id, metadata in books_metadata.items()
+                              if items.issubset(metadata[feature])}
+    if book_object:
+        return create_gutenberg_books(books_metadata, dic=True)
+    return books_metadata
+
+
+def get_bookshelves(bookshelves_list=None):
+    """
+
+    Args:
+        bookshelves_list: (Optional) list of bookshelves to return. if it is None then it will return all bookshelves
+
+    Returns:
+        a dictionary of bookshelves {bookshelf: bookshelf_elements_id}
+
+    """
+    if os.path.isfile(HP.BOOK_SHELVES_PATH):
+        pk = open(HP.BOOK_SHELVES_PATH, "rb")
+        bookshelves = pickle.load(pk)
+        pk.close()
+        assert isinstance(bookshelves, dict)
+        if bookshelves_list is not None:
+            bookshelves_list = bookshelves_list & set(bookshelves)
+            bookshelves = {bookshelf: bookshelves[bookshelf] for bookshelf in bookshelves_list}
+    else:
+        bookshelves = dict()
+    return bookshelves
+
+
+def get_paragraphs(paragraph_id=None, books=None, tags=None, num_sequential=1, Paragraph_Object=True):
+    """
+
+    Get paragraphs from args.
+
+    Args:
+        paragraph_id: (Optional) a list of ints
+        books: (Optional) a list of books id or GutenbergBooks
+        tags: (Optional) a list of tags
+        num_sequential: the number of sequential paragraphs
+        Paragraph_Object: if it is True outputs will be type of Paragraph
+
+    Returns:
+        a list of paragraphs or list of tuples of paragraphs if num_sequential > 1
+
+    """
+    if paragraph_id is not None and (books is not None or tags is not None):
+        raise ValueError("if paragraph_id is given, books and tags can't be accepted.")
+    with open(HP.PARAGRAPH_METADATA_PATH, "rb") as pkl:
+        met_data = pickle.load(pkl)
+    with open(HP.PARAGRAPH_DATA_PATH, "rb") as pkl:
+        text = pickle.load(pkl)
+    pars = create_paragraphs(met_data, text)
+    if paragraph_id is not None:
+        pars = {i: par for i, par in pars.items() if par.id in paragraph_id}
+    if books is not None:
+        books = {i for i in books if isinstance(i, int)} | {book.id for book in books if isinstance(book, GutenbergBook)}
+        pars = {i: par for i, par in pars.items() if par.book_id in books}
+    if tags is not None:
+        pars = {i: par for i, par in pars.items() if par.tags.issuperset(tags)}
+    if num_sequential == 1:
+        return pars.values()
+    assert num_sequential > 1
+    pars2 = []
+    for par in pars.values():
+        pp = [par]
+        next_par = par
+        flag = True
+        for k in range(1, num_sequential):
+            cur_par = next_par
+            id = cur_par.next_id
+            if id not in pars:
+                flag = False
+                break
+            next_par = pars[id]
+            pp.append(next_par)
+        if flag:
+            pars2.append(tuple(pp))
+    return pars2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,39 @@
+# Gutenberg API for paragraph generation
+
+### Preparations
+
+In order to use this API you should get files "books_data.pkl", "books_shelves.pkl", "paragraph_metadata.pkl" and "paragraph_data.pkl" in directory "data/".
+
+
+### API
+
+use API methods such as get_books, get_paragraphs and get_bookshelves to get data. 
+
+GutenbergBook is a class supporting books and Paragraph supporting paragraphs. GutenbergBook contains metadata of each book and Paragraph contain both text and metadata. Metadata of books contains:
+* title
+* authors
+* language 
+* bookshelves: the booshelves that the book belong to.
+
+Paragraph contains book_id, next_id \(next paragraph id\), prev_id, and some tags which illustrate some syntatic properties of paragraph.
+
+
+### get_paragraphs
+
+In order to get a list of paragraphs you should call get_paragraphs:
+```python
+pars = API.get_paragraphs(books=books, tags=tags, num_sequential=n)
+```
+
+* If books is not None, then the returned paragraphs will only belong to that list. books is a list of integers \(books id\) or GutenbergBook.
+* If tags are specified then only the paragraphs which include those tags will be returned. It is recommended to specify that parameter because there are a lot of paragraphs that are not litteraly paragraphs
+\( like dialogue, titles, and ...\). tags are integer numbers and class Tags in HP specify identify each number. For instance HP.Tags.WITHOUT_DIALOGUE (which is 4) identify the paragraphs without any dialogue.
+* If num_sequential > 1, the output will be a list of tuples, triples or ... that are sequential paragraphs each one have the properties given to method. \( being sequential means paragraphs are sequential in a book\) 
+
+Note that you can get paragraphs by paragraph id:
+
+```python
+pars = API.get_paragraphs(paragrap_id=ids)
+```
+
+
